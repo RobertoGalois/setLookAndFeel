e8f69ec5ecd0abee87a6e001f243a85ee410f990
RobertoGalois <victor.vague@live.fr>
2018-04-21 20:02:42 +0200

    Update setLookAndFeel.java

--- setLookAndFeel.java
+++ setLookAndFeel.java
@@ -1,11 +1,12 @@
 private void setLookAndFeel()
 {
-	try {
-		  UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		  SwingUtilities.updateComponentTreeUI(this);
-		}
-		catch (InstantiationException e) {}
-		catch (ClassNotFoundException e) {}
-		catch (UnsupportedLookAndFeelException e) {}
-		catch (IllegalAccessException e) {}
+	try
+	{
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		SwingUtilities.updateComponentTreeUI(this);
+	}
+	catch (InstantiationException e) {}
+	catch (ClassNotFoundException e) {}
+	catch (UnsupportedLookAndFeelException e) {}
+	catch (IllegalAccessException e) {}
 }
